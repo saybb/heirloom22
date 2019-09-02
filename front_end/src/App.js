e77cb35876bafcc4239ff8b345ef3ff9ea556718
saybb <30707324+saybb@users.noreply.github.com>
2019-09-02 19:19:59 +1000

    Create empty Navigation Bar

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -2,9 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+/* Components */
+import Navigation from "./Navigation.js";
+
 function App() {
   return (
     <div className="App">
+      <Navigation />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -21,6 +25,6 @@ function App() {
       </header>
     </div>
   );
-}
+  }
 
 export default App;
