7590914e82356b5356e0946ef8e895bd9736fc81
saybb <30707324+saybb@users.noreply.github.com>
2019-09-03 17:16:03 +1000

    create artefactlist component
    To be completed! Just created the component and added the data.

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -1,28 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 /* Components */
 import Navigation from "./Navigation.js";
+import ArtefactList from "./feed/ArtefactList.js";
 
 function App() {
   return (
     <div className="App">
       <Navigation />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ArtefactList />
     </div>
   );
   }
