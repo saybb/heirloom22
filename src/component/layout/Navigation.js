b4353892ba708db4b135fb8b21927699b1d18ad9
saybb <30707324+saybb@users.noreply.github.com>
2019-09-19 15:27:02 +1000

    separate user profile stuff from navigation

--- src/component/layout/Navigation.js
+++ src/component/layout/Navigation.js
@@ -1,150 +1,57 @@
+/**
+ * Navigation :: ReactJS Component
+ * Renders the navigation bar for the site.
+ * Provides global access to Top-Level links, and allows sign-in and sign-out.
+ */
+
 import React, { Component, Fragment } from 'react'
 import { connect } from 'react-redux'
 import { NavLink } from 'react-router-dom'
-import { Modal, Button } from 'antd';
-import { updateUserProfile, signOut } from '../../store/Actions/userActions'
-import UserProfile from '../profile/userProfile'
-import EditProfile from '../profile/editProfile'
+import { Button } from 'antd';
+import { signOut } from '../../store/Actions/userActions'
+
+import UserModal from '../profile/UserModal.js';
 
 class Navigation extends Component {
 
-  state = {
-    //ModalText: 'Content of the modal',
-    editMode: false,
-    visible: false,
-    confirmLoading: false,
-    firstName: '',
-    lastName: '',
-    location: '',
-    bio: '',
-  };
+    render() {
+        const { auth } = this.props;
 
-  showModal = () => {
-    this.setState({
-      visible: true,
-    });
-  };
-
-  handleEditMode = () => {
-    this.setState({
-      editMode: true,
-    });
-  }
-
-  handleSubmit = (e) => {
-    console.log('updating user profile');
-    this.setState({ 
-      loading: true,  
-      editMode: false
-    });
-    console.log('update: ',this.state.firstName, this.state.lastName, this.state.location, this.state.bio);
-    this.props.updateUserProfile(this.state);
-    setTimeout(() => {
-      this.setState({ loading: false, visible: false });
-    }, 3000);
-  }
-
-  handleBack = () => {
-    this.setState({
-      visible: false,
-      editMode: false
-    });
-  }
-
-  handleCancel = () => {
-    this.setState({
-      editMode: false,
-    });
-  }
-
-  handleChange = (e) => {
-    this.setState({
-      [e.target.id]: e.target.value
-    })
-  }
-
-  render() {
-    const { visible, confirmLoading } = this.state;
-    const { auth, profile } = this.props;
-
-    return(
-      <nav className="Navigation">
-        <div className="container">
-          <h1>Heirloom22 Title Here</h1>
-          { !auth.uid ?
-              <div>
-                <ul className="right">
-                  <li><NavLink to='/'>Home</NavLink></li>
-                  <li><NavLink to='/feed'>List View</NavLink></li>
-                  <li><NavLink to='/signup'>Sign up</NavLink></li>
-                  <li><NavLink to='/signin'>Log in</NavLink></li>
-                  <li><NavLink to='/view/artefact'>Sample Artefact View</NavLink></li>
-                  <li><NavLink to='/view/event'>Sample Event View</NavLink></li>
-                </ul>
-              </div>
-            :
-            <Fragment>
-                <div>
-                  <Button type="primary" onClick={this.showModal}>
-                    Hello, {!auth.displayName ? profile.firstName : auth.displayName}
-                  </Button>
-                  <Modal
-                    title="Profile"
-                    visible={visible}
-                    onOk={this.handleSubmit}
-                    confirmLoading={confirmLoading}
-                    onCancel={this.handleBack}
-                    footer={!this.state.editMode ? 
-                      [
-                        <Button key="close" onClick={this.handleBack}>
-                          Back
-                        </Button>,
-                        <Button key="edit" type="primary" onClick={this.handleEditMode}>
-                          Edit Profile
-                        </Button>,
-                      ]
-                      :
-                      [
-                        <Button key="cancle" onClick={this.handleCancel}>
-                          Return
-                        </Button>,
-                        <Button key="submit" type="primary" onClick={this.handleSubmit}>
-                          Submit
-                        </Button>,
-                      ]
+        return(
+            <nav className="Navigation">
+                <div className="container">
+                    <h1>Heirloom22 Title Here</h1>
+                    { !auth.uid ?
+                        <ul className="right">
+                            <li><NavLink to='/'>Home</NavLink></li>
+                            <li><NavLink to='/feed'>List View</NavLink></li>
+                            <li><NavLink to='/signup'>Sign up</NavLink></li>
+                            <li><NavLink to='/signin'>Log in</NavLink></li>
+                            <li><NavLink to='/view/artefact/family_crest_monument_idnpm '>Sample Artefact View</NavLink></li>
+                            <li><NavLink to='/view/event'>Sample Event View</NavLink></li>
+                        </ul>
+                        :
+                        <Fragment>
+                            <UserModal />
+                            <Button type="danger" onClick={this.props.signOut}>Log out</Button>
+                        </Fragment>
                     }
-                  >
-                    {!this.state.editMode ? 
-                      <UserProfile/>
-                      :
-                      <EditProfile handleChange={this.handleChange} />
-                    }
-                    
-                  </Modal>
                 </div>
-                <div>
-                  <Button type="danger" onClick={this.props.signOut}>Log out</Button>
-                </div>
-              </Fragment>
-          }
-        </div>
-      </nav>
-    )
-  }
+            </nav>
+      )
+    }
 }
 
 const mapStateToProps = (state) => {  
-  return{
-    auth: state.firebase.auth,
-    profile: state.firebase.profile
-  }
+    return{
+        auth: state.firebase.auth
+    }
 }
 
 const mapDispatchToProps = (dispatch) => {
-  return {
-    updateUserProfile: (info) => dispatch(updateUserProfile(info)),
-    signOut: () => dispatch(signOut())
-  }
+    return {
+        signOut: () => dispatch(signOut())
+    }
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(Navigation)
