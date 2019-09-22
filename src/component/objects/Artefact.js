df7026d475b26bb1e28f8906a72c9aa245b7eb78
saybb <30707324+saybb@users.noreply.github.com>
2019-09-22 18:21:56 +1000

    fixed merge fails

--- src/component/objects/Artefact.js
+++ src/component/objects/Artefact.js
@@ -53,14 +53,9 @@ const Artefact = (props) => {
                 <h2>{artefact[id].name}</h2>
                 <p>{artefact[id].details}</p>
                 {/* ItemLinks will render links as items with names and relation descriptors */}
-<<<<<<< HEAD
-                <ItemLinks title="Related People" items={artefact.people_links}/>
-                <ItemLinks title="Related Events" items={artefact.event_links}/>
-                <Addendum />
-=======
                 <ItemLinks title="Related People" items={artefact[id].people_links}/>
                 <ItemLinks title="Related Events" items={artefact[id].events_links}/>
->>>>>>> c8eeb480dce97c5420bff9444362a0ba9588c946
+                <Addendum />
             </div>
         </div>
     )
