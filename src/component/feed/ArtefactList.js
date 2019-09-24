05195dab696bca687cb35796291b47126890e7bc
Luoming Zhang <l.zhang118@student.unimelb.edu.au>
2019-09-24 15:09:39 +1000

    Use React Redux Firebase

--- src/component/feed/ArtefactList.js
+++ src/component/feed/ArtefactList.js
@@ -17,8 +17,6 @@ class ArtefactList extends React.Component {
 
     render() {
         const { artefacts } = this.props;
-        console.log(artefacts)
-
 
         if(!isLoaded(artefacts)){
             return (
