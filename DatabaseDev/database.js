ace471df3728a2e3093267c7999c77abb7b3aba8
chuanyuan <liuchuanyuan@gmail.com>
2019-09-30 08:39:08 +1000

    Cleaned up

--- DatabaseDev/database.js
+++ DatabaseDev/database.js
@@ -5,19 +5,19 @@
 /******************* Document Class ************************/
 /**
  * A class that makes it easier to access information
- * - id 
+ * - id
  *      the document id
- * - data 
+ * - data
  *      the data we are intested in
- * - raw 
+ * - raw
  *      the document reference
  */
-export class Document{
-    constructor(ref) {
-        this.id = ref.id
-        this.data = ref.data()
-        this.ref = ref
-    }
+export class Document {
+   constructor(ref) {
+      this.id = ref.id;
+      this.data = ref.data();
+      this.ref = ref;
+   }
 }
 
 /******************* Generic Class ************************/
@@ -27,102 +27,105 @@ export class Document{
  * 2. get a single data
  *      returns: an object Document
  */
-export class Collection{
+export class Collection {
+   constructor(firestore, name) {
+      this.db = firestore;
+      this.name = name;
+   }
 
-    constructor(firestore, name) {
-        this.db = firestore
-        this.name = name
-    }
+   async get_all_document() {
+      console.log("get_all_documents from " + this.name);
+      try {
+         var snapshot = await this.db.collection(this.name).get();
+         var documents = snapshot.docs;
+         var data = this.grab_data(documents);
+         return data;
+      } catch (error) {
+         console.error("ERROR");
+         return error;
+      }
+   }
 
-    async get_all_document() {
-        console.log("get_all_documents from " + this.name)
-        try {
-            var snapshot = await this.db.collection(this.name).get()
-            var documents = snapshot.docs
-            var data = this.grab_data(documents)
-            return data
-        } catch (error) {
-            console.error("ERROR")
-            return error
-        }
-    };
+   async get_document_by_id(document_id) {
+      console.log(
+         "get_document_by_id from " + this.name + " using " + document_id
+      );
+      try {
+         var doc = await this.db
+            .collection(this.name)
+            .doc(document_id)
+            .get();
+         var data = new Document(doc);
+         return data;
+      } catch (error) {
+         console.error("ERROR");
+         return error;
+      }
+   }
 
-    async get_document_by_id(document_id) {
-        console.log("get_document_by_id from " + this.name + " using " + document_id)
-        try {
-            var doc = await this.db.collection(this.name).doc(document_id).get()
-            var data = new Document(doc)
-            return data
-        } catch (error) {
-            console.error("ERROR")
-            return error
-        }
-    }
+   async upload_document() {}
+   async upload_document_by_id(document_id) {}
 
-    async upload_document() {}
-    async upload_document_by_id(document_id) {}
-
-    grab_data(documents) {
-        var data = []
-        for (const doc of documents) {
-            data.push(new Document(doc))
-        }
-        return data
-    }
+   grab_data(documents) {
+      var data = [];
+      for (const doc of documents) {
+         data.push(new Document(doc));
+      }
+      return data;
+   }
 }
 
 /******************* Individual Collections ************************/
 export class Artifacts_collection extends Collection {
-    constructor(firestore){
-        super(firestore, "Artifacts")
-    }
+   constructor(firestore) {
+      super(firestore, "Artifacts");
+   }
 }
 export class Appendums_collection extends Collection {
-    constructor(firestore){
-        super(firestore, "Addendums")
-    }
+   constructor(firestore) {
+      super(firestore, "Addendums");
+   }
 }
 export class Users_collection extends Collection {
-    constructor(firestore){
-        super(firestore, "Users")
-    }
+   constructor(firestore) {
+      super(firestore, "Users");
+   }
 }
 export class People_collection extends Collection {
-    constructor(firestore){
-        super(firestore, "People")
-    }
+   constructor(firestore) {
+      super(firestore, "People");
+   }
 }
 export class Events_collection extends Collection {
-    constructor(firestore){
-        super(firestore, "Events")
-    }
+   constructor(firestore) {
+      super(firestore, "Events");
+   }
 }
 
 /******************* Convert data ************************/
 function empty(thing) {
-    if (thing === null) {
-        return true
-    }
-    if (thing === undefined) {
-        return true
-    }
-    return false
+   if (thing === null) {
+      return true;
+   }
+   if (thing === undefined) {
+      return true;
+   }
+   return false;
 }
 
 // convert date from string to firebase object
 export function convert_date(text) {
-    if (empty(text)) return null
-    return new firebase.firestore.Timestamp.fromDate(new Date(text))
+   if (empty(text)) return null;
+   return new firebase.firestore.Timestamp.fromDate(new Date(text));
 }
 
 // convert datecreated to server time stamp
 export function server_time_stamp() {
-    return new firebase.firestore.FieldValue.serverTimestamp()
+   return new firebase.firestore.FieldValue.serverTimestamp();
 }
 
 // convert string path to firebase document reference
 export function convert_reference(text, firestore) {
-    if (empty(text)) return null
-    return firestore.doc(text);
+   if (empty(text)) return null;
+   return firestore.doc(text);
 }
-
--- DatabaseDev/storage.js
+++ DatabaseDev/storage.js
@@ -3,26 +3,28 @@
  */
 
 class Image {
-       constructor() {
-
-       }
+   constructor() {}
 }
 
-file_name = "41d7a63.jpg"
+file_name = "41d7a63.jpg";
 
 // Points to the root reference
 var storageRef = firebase.storage().ref();
 
-var pathReference = storage.ref('images/' + file_name);
+var pathReference = storage.ref("images/" + file_name);
 
 function show() {
-    storageRef.child(file_name).getDownloadURL().then(function (url) {
-        // `url` is the download URL for 'images/stars.jpg'
+   storageRef
+      .child(file_name)
+      .getDownloadURL()
+      .then(function(url) {
+         // `url` is the download URL for 'images/stars.jpg'
 
-        // inserted into an <img> element:
-        var img = document.getElementById('myimg');
-        img.src = url;
-    }).catch(function (error) {
-        console.log(error)
-    });
+         // inserted into an <img> element:
+         var img = document.getElementById("myimg");
+         img.src = url;
+      })
+      .catch(function(error) {
+         console.log(error);
+      });
 }
