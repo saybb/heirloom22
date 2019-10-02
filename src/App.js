9417d0bfe973af88b3de18c4d94384f2306c5919
chuanyuan <liuchuanyuan@gmail.com>
2019-10-02 17:23:48 +1000

    welcome page

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@
  * App :: ReactJS Component
  * Main component for the project.
  */
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
-import { connect } from 'react-redux'
-import { compose } from 'redux'
-import './App.css';
-import 'antd/dist/antd.css';
+import React from "react";
+import {
+   BrowserRouter as Router,
+   Route,
+   Switch,
+   Redirect
+} from "react-router-dom";
+import { connect } from "react-redux";
+import { compose } from "redux";
+import "./App.css";
+import "antd/dist/antd.css";
 
 /* Components */
 import Navigation from "./component/layout/Navigation.js";
@@ -15,61 +20,66 @@ import ArtefactList from "./component/feed/ArtefactList.js";
 import Artefact from "./component/objects/Artefact.js";
 import Event from "./component/objects/Event.js";
 import Person from "./component/objects/Person.js";
-import SignIn from './component/auth/SignIn';
-import SignUp from './component/auth/SignUp';
-import Avatar from './component/objects/Avatar';
+import SignIn from "./component/auth/SignIn";
+import SignUp from "./component/auth/SignUp";
+import Avatar from "./component/objects/Avatar";
+import Welcome from "./component/layout/Welcome";
 
 class App extends React.Component {
-    render() {
-        return (
-            <Router>
-                <Navigation />
-                <div className="App">
-                    <this.HomeRoutes />
-                </div>
-            </Router>
-        );
-    };
+   render() {
+      return (
+         <Router>
+            {/* display navigation bar */}
+            <Navigation />
+            {/* display components according to the path specified */}
+            <div className="App">
+               <this.HomeRoutes />
+            </div>
+         </Router>
+      );
+   }
 
-    HomeRoutes = () => {
-        const { auth } = this.props;
+   HomeRoutes = () => {
+      const { auth } = this.props;
 
-        if (auth.uid) {
-            return(
-                <Switch>
-                    <Route path="/" exact component={ArtefactList}/>
-                    <Route path="/feed" component={ArtefactList}/>
-                    <Route path="/view/artefacts/:id" component={Artefact}/>
-                    <Route path="/view/events/:id" component={Event}/>
-                    <Route path="/view/people/:id" component={Person}/>
+      if (auth.uid) {
+         return (
+            // matches the path to the components!!
+            // like a case statement
+            // https://reacttraining.com/react-router/web/guides/quick-start
+            <Switch>
+               <Route path="/" exact component={ArtefactList} />
+               <Route path="/feed" component={ArtefactList} />
+               <Route path="/view/artefacts/:id" component={Artefact} />
+               <Route path="/view/events/:id" component={Event} />
+               <Route path="/view/people/:id" component={Person} />
 
-                    <Route path='/signup' component={SignUp} />
-                    <Route path='/upload' component={Avatar} />
+               <Route path="/signup" component={SignUp} />
+               <Route path="/upload" component={Avatar} />
+               <Route path="/welcome" component={Welcome} />
 
-                    {/* default to "/" if unrecognised route. */}
-                    <Route render={() => <Redirect to="/"/>}/>
-                </Switch>
-            );
-        } else {
-            return(
-                <Switch>
-                    {/* Welcome page goes here... */}
-                    <Route path='/' exact component={SignIn} />
-                    <Route path='/signin' component={SignIn} />
+               {/* default to "/" if unrecognised route. */}
+               <Route render={() => <Redirect to="/" />} />
+            </Switch>
+         );
+      } else {
+         return (
+            <Switch>
+               {/* Welcome page goes here... */}
+               <Route path="/" exact component={SignIn} />
+               <Route path="/signin" component={SignIn} />
 
-                    {/* default to "/" if unrecognised route. */}
-                    <Route render={() => <Redirect to="/"/>}/>
-                </Switch>
-            )
-        }
-    }
-}
-
-const mapStateToProps = (state) => {   
-      return {
-          auth: state.firebase.auth,
+               {/* default to "/" if unrecognised route. */}
+               <Route render={() => <Redirect to="/" />} />
+            </Switch>
+         );
       }
+   };
 }
-export default compose(
-    connect(mapStateToProps)
-)(App)
+
+const mapStateToProps = state => {
+   return {
+      auth: state.firebase.auth
+   };
+};
+export default compose(connect(mapStateToProps))(App);
--- src/component/layout/Navigation.js
+++ src/component/layout/Navigation.js
@@ -4,92 +4,122 @@
  * Provides sitewide navigation, and access to account features.
  */
 
-import React from 'react'
-import { connect } from 'react-redux'
-import { NavLink } from 'react-router-dom'
-import { Menu, Button } from 'antd';
-import { signOut } from '../../store/Actions/authActions'
-import logo from '../../heirloom22_logo.svg';
-import UserModal from '../profile/UserModal.js';
-import './Navigation.css'
+import React from "react";
+import { connect } from "react-redux";
+import { NavLink } from "react-router-dom";
+import { Menu, Button } from "antd";
+import { signOut } from "../../store/Actions/authActions";
+import logo from "../../heirloom22_logo.svg";
+import UserModal from "../profile/UserModal.js";
+import "./Navigation.css";
 
 class Navigation extends React.Component {
-    render() {
-        return(
-            <nav className="nav-bar">
-                <this.MainMenu />
-                <this.UserMenu />
-            </nav>
-        );
-    }
+   render() {
+      return (
+         <nav className="nav-bar">
+            <this.MainMenu />
+            <this.UserMenu />
+         </nav>
+      );
+   }
 
-    // menu for site navigation
-    MainMenu = () => {
-        const { auth } = this.props;
+   // menu for site navigation
+   MainMenu = () => {
+      const { auth } = this.props;
 
-        if (auth.uid) {
-            return(
-                <Menu className="menu-main" mode="horizontal">
-                    <Menu.Item key="logo">
-                        <NavLink to='/'>
-                            <img className="nav-logo" src={logo} alt="logo"/>
-                        </NavLink>
-                    </Menu.Item>
-                    <Menu.Item key="home"><NavLink to='/'>Home</NavLink></Menu.Item>
-                    <Menu.Item key="list"><NavLink to='/feed'>List View</NavLink></Menu.Item>
-                    <Menu.Item key="artefact"><NavLink to='/view/artefacts/family_crest_monument_id'>Sample Artefact View</NavLink></Menu.Item>
-                    <Menu.Item key="event"><NavLink to='/view/events/making_crest_id'>Sample Event View</NavLink></Menu.Item>
-                    <Menu.Item key="person"><NavLink to='/view/people/john_gilbert_id'>Sample Person View</NavLink></Menu.Item>
-                </Menu>
-            );
-        } else {
-            return(
-                <Menu className="menu-main" mode="horizontal">
-                    <Menu.Item key="logo">
-                        <NavLink to='/'>
-                            <img className="nav-logo" src={logo} alt="logo"/>
-                        </NavLink>
-                    </Menu.Item>
-                    <Menu.Item key="home"><NavLink to='/'>Home</NavLink></Menu.Item>
-                </Menu>
-            );
-        }
-    }
+      // use auth.uid to check if we can show the signup page
+      if (auth.uid) {
+         return (
+            <Menu className="menu-main" mode="horizontal">
+               <Menu.Item key="logo">
+                  <NavLink to="/">
+                     <img className="nav-logo" src={logo} alt="logo" />
+                  </NavLink>
+               </Menu.Item>
+               <Menu.Item key="home">
+                  <NavLink to="/">Home</NavLink>
+               </Menu.Item>
+               <Menu.Item key="list">
+                  <NavLink to="/feed">List View</NavLink>
+               </Menu.Item>
+               <Menu.Item key="artefact">
+                  <NavLink to="/view/artefacts/family_crest_monument_id">
+                     Sample Artefact View
+                  </NavLink>
+               </Menu.Item>
+               <Menu.Item key="event">
+                  <NavLink to="/view/events/making_crest_id">
+                     Sample Event View
+                  </NavLink>
+               </Menu.Item>
+               <Menu.Item key="person">
+                  <NavLink to="/view/people/john_gilbert_id">
+                     Sample Person View
+                  </NavLink>
+               </Menu.Item>
+            </Menu>
+         );
+      } else {
+         return (
+            <Menu className="menu-main" mode="horizontal">
+               <Menu.Item key="logo">
+                  <NavLink to="/">
+                     <img className="nav-logo" src={logo} alt="logo" />
+                  </NavLink>
+               </Menu.Item>
+               <Menu.Item key="home">
+                  <NavLink to="/">Home</NavLink>
+               </Menu.Item>
+            </Menu>
+         );
+      }
+   };
 
-    // menu for account-related functions
-    UserMenu = () => {
-        const { auth, signOut } = this.props;
+   // menu for account-related functions
+   UserMenu = () => {
+      const { auth, signOut } = this.props;
 
-        if (auth.uid) {
-            return(
-                <Menu className="menu-user" mode="horizontal">
-                    <Menu.Item key="profile"><UserModal /></Menu.Item>
-                    <Menu.Item key="logout"><Button type="danger" onClick={signOut}>Log out</Button></Menu.Item>
-                    <Menu.Item key="signup"><NavLink to='/signup'>Sign up</NavLink></Menu.Item>
-                </Menu>
-            );
-        } else {
-            return(
-                <Menu className="menu-user" mode="horizontal">
-                    <Menu.Item key="login"><NavLink to='/signin'>Log in</NavLink></Menu.Item>
-                </Menu>
-            );
-
-        }
-
-    }
+      if (auth.uid) {
+         return (
+            <Menu className="menu-user" mode="horizontal">
+               <Menu.Item key="profile">
+                  <UserModal />
+               </Menu.Item>
+               <Menu.Item key="logout">
+                  <Button type="danger" onClick={signOut}>
+                     Log out
+                  </Button>
+               </Menu.Item>
+               <Menu.Item key="signup">
+                  <NavLink to="/signup">Sign up</NavLink>
+               </Menu.Item>
+            </Menu>
+         );
+      } else {
+         return (
+            <Menu className="menu-user" mode="horizontal">
+               <Menu.Item key="login">
+                  <NavLink to="/signin">Log in</NavLink>
+               </Menu.Item>
+            </Menu>
+         );
+      }
+   };
 }
 
-const mapStateToProps = (state) => {  
-    return{
-        auth: state.firebase.auth
-    }
-}
+const mapStateToProps = state => {
+   return {
+      auth: state.firebase.auth
+   };
+};
 
-const mapDispatchToProps = (dispatch) => {
-    return {
-        signOut: () => dispatch(signOut())
-    }
-}
+const mapDispatchToProps = dispatch => {
+   return {
+      signOut: () => dispatch(signOut())
+   };
+};
 
-export default connect(mapStateToProps, mapDispatchToProps)(Navigation)
+export default connect(
+   mapStateToProps,
+   mapDispatchToProps
+)(Navigation);
