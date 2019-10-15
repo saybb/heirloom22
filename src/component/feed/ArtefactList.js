495de7083c52caeedfca649733e06dc3254be174
Luoming Zhang <lokissue@gmail.com>
2019-10-16 10:59:59 +1100

    List view header

--- src/component/feed/ArtefactList.js
+++ src/component/feed/ArtefactList.js
@@ -8,6 +8,7 @@ import React from 'react';
 import { connect } from 'react-redux'
 import { firestoreConnect } from 'react-redux-firebase'
 import { compose } from 'redux'
+import { PageHeader, Tag } from 'antd';
 
 // components
 import ArtefactListElement from "./ArtefactListElement.js";
@@ -18,7 +19,7 @@ import { ARTEFACTS } from "../../store/objectTypes";
 class ArtefactList extends React.Component {
     
     render() {
-        const { artefacts } = this.props;
+        const { artefacts, profile } = this.props;
         if(!artefacts){
             return (
                 <div className="container center">
@@ -34,13 +35,21 @@ class ArtefactList extends React.Component {
             )
         }
         return(
-            <div className="artefact-list-wrapper">
-                <div className="artefact-list-header">
-                    <h2>Browsing your collection...</h2>
-                    <ArtefactHandler type={"create"} />
-                </div>
-                <div className="artefact-list">
-                    { artefacts && Object.entries(artefacts).map(([id, artefact]) => <ArtefactListElement key={id} reference={id} artefact={artefact}/>)}  
+            <div>
+                <PageHeader
+                    onBack={() => window.history.back()}
+                    title="Browsing your collection"
+                    tags={<Tag color="blue">{profile.name}</Tag>}
+                    extra={[
+                        <ArtefactHandler type={"create"} />
+                    ]}
+                    avatar={{ src: profile.photoURL }}
+                >
+                </PageHeader>
+                <div className="artefact-list-wrapper">
+                    <div className="artefact-list">
+                        { artefacts && Object.entries(artefacts).map(([id, artefact]) => <ArtefactListElement key={id} reference={id} artefact={artefact}/>)}  
+                    </div>
                 </div>
             </div>
         )
@@ -51,7 +60,7 @@ const mapStateToProps = (state) => {
     return {
       artefacts: state.firestore.data.Artefacts,
       auth: state.firebase.auth,
-      // profile: state.firebase.profile,
+      profile: state.firebase.profile,
     }
 }
   
