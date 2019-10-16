a6abee09cda151b0c1dc2893cb387f66596f247c
Luoming Zhang <lokissue@gmail.com>
2019-10-16 16:26:51 +1100

    add delete buttons

--- src/component/profile/userProfile.js
+++ src/component/profile/userProfile.js
@@ -1,22 +1,30 @@
-import React, { Fragment} from 'react'
+import React, { Fragment, useState} from 'react'
 import { Redirect } from 'react-router-dom'
 import { connect } from 'react-redux'
-
+import { Icon, Spin } from 'antd';
 
 const UserProfile = (props) => {
+    const [loaded, setLoaded] = useState(false);
     const { auth, profile } = props;
+    const loadingIcon = <Icon type="loading" style={{ fontSize: 24 }} spin />
     if (!auth.uid) { return <Redirect to='/signin' /> }
     
     return (
         <Fragment>
-            <img src= {profile.photoURL ? profile.photoURL : "https://firebasestorage.googleapis.com/v0/b/testing-e1ec1.appspot.com/o/No%20profile%20photo.png?alt=media&token=23fa70a3-c8a7-409d-bd73-0d38ed8772d6"}
-                alt="" 
-                style={{width: 200,
-                    height: 200,
-                    objectFit: 'cover',
-                    maxWidth: '100%',
-                    borderRadius: '50%',
-                    verticalAlign: 'middle'}}/>
+            <div>
+            {loaded ? null :
+            <Spin indicator={loadingIcon} />
+            }
+                <img src= {profile.photoURL ? profile.photoURL : "https://firebasestorage.googleapis.com/v0/b/testing-e1ec1.appspot.com/o/No%20profile%20photo.png?alt=media&token=23fa70a3-c8a7-409d-bd73-0d38ed8772d6"}
+                    alt="" 
+                    style={{width: 200,
+                        height: 200,
+                        objectFit: 'cover',
+                        maxWidth: '100%',
+                        borderRadius: '50%',
+                        verticalAlign: 'middle'}}
+                        onLoad={() => setLoaded(true)}/>
+            </div>
             <p> {auth.email}</p>
             <p>{profile.name}  {profile.lastName} </p>
             <p>{profile.email}</p>
